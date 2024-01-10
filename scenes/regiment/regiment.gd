e5e82b4b05baa0550631556fd36eee88ff5c7d5a
Sebastian R. Valdivia <75336522+SebastianRValdivia@users.noreply.github.com>
2024-01-10 20:51:10 -0300

    spawn soldiers in regiment area

--- scenes/regiment/regiment.gd
+++ scenes/regiment/regiment.gd
@@ -16,6 +16,15 @@ func _ready():
 		var soldier_scene = preload("res://scenes/soldier/soldier.tscn")
 		for i in range(soldiers_count):
 			var soldier_instance = soldier_scene.instantiate()
-			soldier_instance.position = Vector2(i, i)
+			soldier_instance.position = Vector2(
+				randi_range(
+					$RegimentArea/CollisionShape2D.position.x,
+					$RegimentArea/CollisionShape2D.shape.size.x
+				),
+				randi_range(
+					$RegimentArea/CollisionShape2D.position.y,
+					$RegimentArea/CollisionShape2D.shape.size.y
+				)
+			)
 			add_child(soldier_instance)
 	
